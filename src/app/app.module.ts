bf724d84a1a27ee2dc429db200e543e6e7122313
Diego Soto <charsh151@gmail.com>
2024-10-26 19:59:44 -0300

    Se agregaron tres imágenes por destino y además cada destino tiene un texto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+/* import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
@@ -17,6 +17,30 @@ import { DestinationsComponent } from './destinations/destinations.component';
     HttpClientModule // Importa HttpClientModule aquí
   ],
   providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
+ */
+
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http'; // Asegúrate de importar provideHttpClient y withFetch
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DestinationsComponent } from './destinations/destinations.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DestinationsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideHttpClient(withFetch()) // Configuración para habilitar fetch en HttpClient
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/destinations/destinations.service.ts
+++ src/app/destinations/destinations.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/* import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -17,3 +17,26 @@ export class DestinationsService {
 }
 
 
+ */
+
+/* 
+  código para que se muestren dos imágenes al mismo tiempo */
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DestinationsService {
+  private accessKey = '00bORbNasXiU6iQ5bkxoTHhDk9oT7CQZ4eSuPkrzlSU';
+  private apiUrl = `https://api.unsplash.com/search/photos`;
+
+  constructor(private http: HttpClient) {}
+
+  getImages(destination: string): Observable<any> {
+    return this.http.get(`${this.apiUrl}?query=${destination}&client_id=${this.accessKey}&per_page=3`);
+    /* return this.http.get(`${this.apiUrl}?query=${destination}&client_id=${this.accessKey}&per_page=2`); */
+  }
+}
